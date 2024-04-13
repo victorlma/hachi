b7d15221df8e985fc6ebc54f0977ff7f29b66526
Victorlma <victorlima1414x@gmail.com>
2024-04-13 20:36:22 -0300

    Ncur setupkey

---
+++ src/ncur_layer.c
@@ -0,0 +1,22 @@
+#include "ncurses.h"
+#include "ncur_layer.h"
+
+void ncur_setupkeys()
+{ 
+    Hachi.k.b0.key = '1' ;
+    Hachi.k.b1.key = '2' ;
+    Hachi.k.b2.key = '3' ;
+    Hachi.k.b3.key = '4' ;
+    Hachi.k.b4.key = 'q' ;
+    Hachi.k.b5.key = 'w' ;
+    Hachi.k.b6.key = 'e' ;
+    Hachi.k.b7.key = 'r' ;
+    Hachi.k.b8.key = 'a' ;
+    Hachi.k.b9.key = 's' ;
+    Hachi.k.ba.key = 'd' ;
+    Hachi.k.bb.key = 'f' ;
+    Hachi.k.bc.key = 'z' ;
+    Hachi.k.bd.key = 'x' ;
+    Hachi.k.be.key = 'c' ;
+    Hachi.k.bf.key = 'v' ;
+}
---
+++ src/ncur_layer.h
@@ -0,0 +1,5 @@
+#ifndef NCURL_H_
+#define NCURL_H_
+
+void ncur_setupkeys();
+#endif
