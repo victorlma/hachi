7bb4d4c2858a7899976a0ce5413292481c90975a
Victorlma <victorlima1414x@gmail.com>
2024-05-03 17:18:11 -0300

    Fps Cycles refactored

--- src/main.c
+++ src/main.c
@@ -1,11 +1,9 @@
-#define _XOPEN_SOURCE   600
-#define _POSIX_C_SOURCE 200112L
+#define _POSIX_C_SOURCE 199309L
 #include "locale.h"
 #include "stdio.h"
 #include "stdlib.h"
 
 #include "time.h"
-#include "unistd.h"
 #include "ncurses.h"
 
 #include "hachi.h"
@@ -23,8 +21,6 @@
 int main(int argc, char **argv)
 {
 
-    float targetFPS = (1.0f/60.0f);
-
     if (argc < 2) return 1;
 
     setlocale(LC_ALL, "");
@@ -36,6 +32,7 @@ int main(int argc, char **argv)
 
     int offset = 0;
     int execIns = 0;
+
     for (int i=0; i < filesize; ++i)
     {
         unsigned char bf[2];
@@ -48,6 +45,11 @@ int main(int argc, char **argv)
     
     
 
+    float fps = 60;
+    float target = 1.0f/fps;
+
+    struct timespec tspec = {0};
+
     setuphachi();
 
 
@@ -58,20 +60,24 @@ int main(int argc, char **argv)
         handleResize();
 
         decodeAndExec(fetchIns());
-        execIns++;
 
 
         clk = clock() - clk;
 
-        if (targetFPS >  (float)clk /CLOCKS_PER_SEC && !(execIns < Hachi.insPerFrame))
+        float ms = (float) clk/CLOCKS_PER_SEC;
+        
+        if (ms < target && ++execIns >= Hachi.insPerFrame)
         {
+            tspec.tv_sec = 0;
+            tspec.tv_nsec = (target - ms) * 1000000000L;
             execIns = 0;
-            usleep( (targetFPS - (float) clk / CLOCKS_PER_SEC) * 1000000);
-            
+
+            nanosleep(&tspec, &tspec);
         }
 
-        mvprintw(4, 0, "%f",(1-targetFPS - (float) clk / CLOCKS_PER_SEC)*60);
-        mvprintw(5, 0, "InsPerFrame: %d",Hachi.insPerFrame);
+
+        clk = clock();
+
     }
 
     atexit(cleanup);
